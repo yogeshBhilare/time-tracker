b911d08bf25c65a3b2a697fe883b77a140e4616e
yogeshBhilare <58133704+yogeshBhilare@users.noreply.github.com>
2019-12-15 00:26:36 +0530

    correcting code

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -16,10 +16,7 @@ public class Tracker {
     }
 
     public void remove(TimeEntry entry) {
-        if (true)
-            entries.remove(entry);
-
-        entries.remove(entry);
+         entries.remove(entry);
     }
 
     public int size() {
@@ -27,18 +24,6 @@ public class Tracker {
     }
 
     public TimeEntry get(int index) {
-        try {
-
-        } catch (Exception e) {
-
-        }
-
-        boolean valid = false;
-
-        if (valid = true) {
-            // whatever
-        }
-
         return entries.get(index);
     }
 }
